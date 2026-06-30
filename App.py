3041c720c24433c768c2538e65ed0cc0bbb479ee
hventas476-hash <hventas476@gmail.com>
2026-06-29 21:35:37 -0500

    App.py

--- App.py
+++ App.py
@@ -1,18 +1,25 @@
 import streamlit as st
 import joblib
-import gdown # Esta librería descarga archivos de Drive
-import os 
-# Enlace de tu archivo en Drive (el que copiaste)
-url_modelo = 'https://drive.google.com/file/d/19xoL1Tr5KgDT566paLuimCwHCzWY-JbO/view?usp=drivesdk'
-output = 'modelo_futbol.pkl'
+import gdown
+import os
 
-# Descargar si no existe
-if not os.path.exists(output):
-    gdown.download(url_modelo, output, quiet=False)
+# 1. Descargar Modelo
+url_modelo = 'https://drive.google.com/file/d/19xoi1Tr5KgDTS66paiuimCwHCwY-JB0/view?usp=sharing'
+out_modelo = 'modelo_futbol.pkl'
+if not os.path.exists(out_modelo):
+    gdown.download(url_modelo, out_modelo, quiet=False, use_cookies=False)
 
-# Cargar el modelo
-modelo = joblib.load('modelo_futbol.pkl')
+# 2. Descargar Codificador (¡Es vital!)
+url_cod = 'AQUÍ_VA_EL_ENLACE_DE_TU_CODIFICADOR_EN_DRIVE'
+out_cod = 'codificador.pkl'
+if not os.path.exists(out_cod):
+    gdown.download(url_cod, out_cod, quiet=False, use_cookies=False)
+
+# 3. Cargar ambos
+modelo = joblib.load(out_modelo)
+codificador = joblib.load(out_cod)
 
 st.title("⚽ Predictor de Fútbol")
-# ... resto de tu código de Streamlit ...
+st.write("¡Modelo cargado correctamente!")
+
 
