ca418f12a9e680179e7981d60885da75b5a4ceca
hventas476-hash <hventas476@gmail.com>
2026-06-29 21:19:59 -0500

    Create App.py

---
+++ App.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+import gdown # Esta librería descarga archivos de Drive
+
+# Enlace de tu archivo en Drive (el que copiaste)
+url_modelo = 'https://drive.google.com/file/d/19xoL1Tr5KgDT566paLuimCwHCzWY-JbO/view?usp=drivesdk'
+output = 'modelo_futbol.pkl'
+
+# Descargar si no existe
+if not os.path.exists(output):
+    gdown.download(url_modelo, output, quiet=False)
+
+# Cargar el modelo
+modelo = joblib.load('modelo_futbol.pkl')
+
+st.title("⚽ Predictor de Fútbol")
+# ... resto de tu código de Streamlit ...
+
