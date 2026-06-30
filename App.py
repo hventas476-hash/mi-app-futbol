e507092a65a05cc77eadd887cb13ba1d9c5c0b67
hventas476-hash <hventas476@gmail.com>
2026-06-29 21:44:44 -0500

    App.py

--- App.py
+++ App.py
@@ -9,8 +9,9 @@ out_modelo = 'modelo_futbol.pkl'
 if not os.path.exists(out_modelo):
     gdown.download(url_modelo, out_modelo, quiet=False, use_cookies=False)
 
-# 2. Descargar Codificador (¡Es vital!)
-url_cod = 'AQUÍ_VA_EL_ENLACE_DE_TU_CODIFICADOR_EN_DRIVE'
+# 2. Descargar Codificador
+# AQUÍ PEGA EL ENLACE QUE COPIASTE DEL PASO 1
+url_cod = 'https://drive.google.com/file/d/1DjGjbOqI57KdFBz_2UT2m5ujVRiOUQaj/view?usp=drivesdk' 
 out_cod = 'codificador.pkl'
 if not os.path.exists(out_cod):
     gdown.download(url_cod, out_cod, quiet=False, use_cookies=False)
@@ -20,6 +21,5 @@ modelo = joblib.load(out_modelo)
 codificador = joblib.load(out_cod)
 
 st.title("⚽ Predictor de Fútbol")
-st.write("¡Modelo cargado correctamente!")
-
+st.write("¡Modelo y codificador cargados correctamente!")
 
